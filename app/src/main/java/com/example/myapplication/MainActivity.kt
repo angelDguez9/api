460cc9b1d41a0fc68705993250da4ed5adc32a6d
BorjasRaul <79670821+BorjasRaul@users.noreply.github.com>
2021-02-28 15:06:55 -0600

    messaje

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
 
-
+//hola
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
