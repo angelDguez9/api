f57aa166f914ffcade7643d303a72dde7618441f
angelDguez9 <79721414+angelDguez9@users.noreply.github.com>
2021-02-28 15:13:22 -0600

    "Api2"

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainViewModel
 
 //hola
+    //rulo el perro
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
